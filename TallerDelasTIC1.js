57748e59b053a7c991b1bc28fd012c0c23242871
Michael1323 <michaeljesus2006@gmail.com>
2024-12-02 08:07:09 -0500

    Laboratorio #1

---
+++ TallerDelasTIC1.js
@@ -0,0 +1,22 @@
+let nombre="Michael";
+let edad=18;
+let esEstudiante=false;
+
+console.log("Nombre: "+nombre);
+console.log("Edad: ",edad);
+console.log(`Es estidante: ${esEstudiante}`);
+
+//
+
+let a=10
+let b=10
+
+let suma=a+b;
+let resta=a-b;
+let mult=a*b;
+let divd=a/b;
+
+console.log("Suma: "+suma);
+console.log("resta: "+resta);
+console.log("mult:"+mult);
+console.log("divd:"+divd);
